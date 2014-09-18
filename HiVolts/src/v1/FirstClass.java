fc1704f5eb57c8e7eb880cf732be9cacef559587
Peter <pcturnbull7@gmail.com>
2014-09-18 13:19:05 -0700

    Building the Class
    Put the class together, added some filler comments at the top. Work soon
    to come!

---
+++ HiVolts/src/v1/FirstClass.java
@@ -0,0 +1,23 @@
+/* Hello! The authors of this code are: Mischa Laava, Peter Turnbull, and Przemek Gardias
+ * 
+ * Other contributors/helpers:
+ * 
+ * Sources of code:
+ * 
+ * Problems We Encountered:
+ * 
+ * Decisions We Made:
+ * 
+ * Assumptions We Made in the Bleak Storm of Ambiguity
+ * 
+ * Justifications for Choices We Made That Resulted in Fewer Features Than the Requirements Call For:
+ * 
+ */
+
+package v1;
+
+public class FirstClass {
+	public static void main(String[] args) {
+		System.out.println("It's alive!");
+	}
+}
