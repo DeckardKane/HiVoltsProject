1bc4dfbf079bf406ca72d7d6d2eaa35dc2f955a6
8Awesome <michlavva97@gmail.com>
2014-09-23 13:53:12 -0700

    Added Smiley and Created Class Cell

---
+++ HiVolts/src/v1/Cell.java
@@ -0,0 +1,25 @@
+package v1;
+import java.awt.Color;
+import java.awt.Graphics; 
+import javax.swing.ImageIcon;
+
+public class Cell {
+	private int myX, myY;
+	private Color myColor;
+	private final Color DEFAULT_ALIVE = Color.ORANGE;
+	private final Color DEFAULT_DEAD = Color.GRAY;
+	private String type;
+	ImageIcon Smiley = new ImageIcon("Smiley.png") 
+	
+//	if(Type == "Smiley"){
+//		
+//	}
+		
+	//Kuszmaul's Example. Not needed right now. 
+	private int RandomNumberInRange(int start, int end){
+		double randd = Math.random();
+		randd *= (end-start + 1);
+		randd += start; 
+		return (int) randd; 
+	}
+}
