764b3b3af48e7672adc38cee14da142e64c28e60
Peter <pcturnbull7@gmail.com>
2014-09-29 14:29:05 -0700

    MhoControl Skeleton
    All I could do in this short time, set up a base for the Mho AI.

---
+++ HiVolts/src/v1/MhoControl.java
@@ -0,0 +1,22 @@
+package v1;
+
+public class MhoControl {
+	int mhoX;
+	int mhoY;
+	
+	public int getMhoX() {
+		return mhoX;
+	}
+	public void setMhoX(int mhoX) {
+		this.mhoX = mhoX;
+	}
+	public int getMhoY() {
+		return mhoY;
+	}
+	public void setMhoY(int mhoY) {
+		this.mhoY = mhoY;
+	}
+	
+	
+	
+}
