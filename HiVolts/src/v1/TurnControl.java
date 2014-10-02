a4fce88ee843d8867805131c8f90e19fa6505055
Peter <pcturnbull7@gmail.com>
2014-10-02 13:46:13 -0700

    Changes to Turn Control
    I'm thinking that this class might get scrapped and we can refactor
    HVListener to MovementControl or something, it has taken a lot of this
    class' features anyway.

--- HiVolts/src/v1/TurnControl.java
+++ HiVolts/src/v1/TurnControl.java
@@ -2,32 +2,15 @@ package v1;
 
 public class TurnControl {
 	//Game Values
-	boolean playerTurn = true;
-	//Mho values
-	int mhoX;
-	int mhoY;
-	
-	
+	static boolean playerTurn = true;
+
 	public boolean getPlayerTurn() {
 		return playerTurn;
 	}
 	public void setPlayerTurn(boolean playerTurn) {
 		this.playerTurn = playerTurn;
 	}
-	
-	public int getMhoX() {
-		return mhoX;
-	}
-	public void setMhoX(int mhoX) {
-		this.mhoX = mhoX;
-	}
-	public int getMhoY() {
-		return mhoY;
-	}
-	public void setMhoY(int mhoY) {
-		this.mhoY = mhoY;
-	}
-	
+
 	public void turnControl(){
 		if (playerTurn == true) {
 			System.out.println("It's the player's turn.");
