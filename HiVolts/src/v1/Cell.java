35fe189dde1a09a0a701adc8f3606fcb8b66fa92
Misha Lavva <michlavva97@gmail.com>
2014-10-05 20:20:03 -0700

    Redid Cell. This will work with New Display.
    This might have a lot of conflicts, but this code is good.

--- HiVolts/src/v1/Cell.java
+++ HiVolts/src/v1/Cell.java
@@ -7,62 +7,16 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Cell extends JPanel {
-	private int myX, myY;
-
-	private Color myColor;
-	private boolean myAlive;
-
-	private String myType;
-
-	private final Color SMILEY = Color.ORANGE;
-	private final Color FENCE = Color.GRAY;
-	private final Color MHO = Color.GREEN;
-
-	ImageIcon Smiley = new ImageIcon("Smiley.png");
-	ImageIcon Sad = new ImageIcon("SadFace.png");
-	ImageIcon Fence = new ImageIcon("Fence.jpg");
-
-	public Cell(int x, int y) {
-		this(x, y, Color.BLACK, null);
-	}
-
-	public Color getColor() {
-		return myColor;
-	}
-
-	public Cell(int row, int col, Color color, String type) {
-		myType = type;
-		myX = col;
-		myColor = color;
-		myY = row;
-	}
-
-	public void setSmiley(Boolean Smiley) {
-		if (Smiley == true) {
-			myColor = SMILEY;
-		}
-	}
-	public void setFence (Boolean Fence) {
-		if (Fence == true) {
-			myColor = FENCE;
-		}
-	}
-	public void setMho (Boolean Mho) {
-		if (Mho == true) {
-			myColor = MHO;
-		}
-	}
 	
-	public String getType() {
-		return myType;
+	private int myX, myY; // x,y position on grid
+	private Color myColor; // Based on alive/dead rules
+
+	public Cell(int x, int y, Color color) {
+		myColor = color;
+		myX = x;
+		myY = y;
 	}
 
-	/*
-	 * public void setType(String Type, Graphics g){ myType = Type; if(myType ==
-	 * "Smiley"){ super.paintComponent(g); Smiley.paintIcon(Smiley, g, 10, 10);
-	 * } if(myType == "Sad"){ setImage("SadFace.png"); } if(myType == "Fence"){
-	 * setImage("Fence.jpg"); } }
-	 */
 	public int getX() {
 		return myX;
 	}
@@ -71,14 +25,32 @@ public class Cell extends JPanel {
 		return myY;
 	}
 
-	// Kuszmaul's Example. Not needed right now.
-	public void draw(int xOffset, int yOffset, int width, int height, Graphics g) {
-		// I leave this understanding to the reader.
-		int xLeft = xOffset + 1 + (myX * (width + 1));
-		int yTop = yOffset + 1 + (myY * (height + 1));
-		g.setColor(myColor);
-		g.fillRect(xLeft, yTop, width, height);
+	public Color getColor() {
+		return myColor;
+	}
+
+	public void setX(int x) {
+		myX = x;
+	}
+
+	public void setY(int y) {
+		myY = y;
+	}
+
+	public void setColor(Color color) {
+		myColor = color;
+	}
+
+	public void draw(int x_offset, int y_offset, int width, int height,
+			Graphics g) {
+		draw(myX, myY, x_offset, y_offset, width, height, myColor, g);
+	}
+
+	public static void draw(int x, int y, int x_offset, int y_offset,int width, int height, Color c, Graphics g) {
+		int xleft = x_offset + 1 + (x * (width + 1));
+		int ytop = y_offset + 1 + (y * (height + 1));
+		g.setColor(c);
+		g.fillRect(xleft, ytop, width, height);
 	}
 }
-
 
