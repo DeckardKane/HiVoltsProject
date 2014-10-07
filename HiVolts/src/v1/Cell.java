e0dffe70a798de50746c07a5c6c19dba3d6fc6d0
Misha Lavva <michlavva97@gmail.com>
2014-10-07 16:31:25 -0700

    Part of Image Implementation
    Required to have this.

--- HiVolts/src/v1/Cell.java
+++ HiVolts/src/v1/Cell.java
@@ -2,6 +2,7 @@ package v1;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -10,11 +11,14 @@ public class Cell extends JPanel {
 	
 	private int myX, myY; // x,y position on grid
 	private Color myColor; // Based on alive/dead rules
+	private Image myImage; // optional image
 
-	public Cell(int x, int y, Color color) {
+	public Cell(int x, int y, Color color, Image image) {
 		myColor = color;
 		myX = x;
 		myY = y;
+		myImage = image;
+		
 	}
 
 	public int getX() {
@@ -43,13 +47,17 @@ public class Cell extends JPanel {
 
 	public void draw(int x_offset, int y_offset, int width, int height,
 			Graphics g) {
-		draw(myX, myY, x_offset, y_offset, width, height, myColor, g);
+		draw(myX, myY, x_offset, y_offset, width, height, myColor, g, myImage);
 	}
 
-	public static void draw(int x, int y, int x_offset, int y_offset,int width, int height, Color c, Graphics g) {
+	public static void draw(int x, int y, int x_offset, int y_offset,int width, int height, Color c, Graphics g, Image image) {
 		int xleft = x_offset + 1 + (x * (width + 1));
 		int ytop = y_offset + 1 + (y * (height + 1));
 		g.setColor(c);
-		g.fillRect(xleft, ytop, width, height);
+		if (image == null) {
+			g.fillRect(xleft, ytop, width, height);
+		} else {
+			g.drawImage(image, xleft, ytop, width, height, c, null);
+		}
 	}
 }
